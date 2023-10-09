961de248a3bf9195401e3d9e6c8b5daa27a47129
JocelynDeloose <deloosejocelyn@gmail.com>
2023-10-09 17:21:35 +0200

    first

---
+++ src/main/java/com/wild/MyProject/MyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wild.MyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectApplication.class, args);
+	}
+
+}
